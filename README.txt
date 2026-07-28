356a1a7ebbedb267c6ace9893c72fe65d0f16362
bubsmilo <bubsmilo@gmail.com>
2026-07-28 10:55:20 -0400

    Add files via upload

--- README.txt
+++ README.txt
@@ -11,3 +11,5 @@ Included:
 - favicon.png
 
 The app stores data on the current device using localStorage.
+
+Version 1.1 adds a Settings screen with Share App and Copy App Link.
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'the-crease-v1';
+const CACHE = 'the-crease-v1-1';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./favicon.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
