54f2d8f94863cfccac5447e32eaa9e98f6dbf3fe
bubsmilo <bubsmilo@gmail.com>
2026-07-28 12:17:23 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Paste the Firebase web app configuration here.
 // Firebase Console > Project settings > Your apps > Web app > SDK setup and configuration
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyCLHKJWdlnMJwnzDkClojOA7Ydy-jgY0QQ",
+  authDomain: "the-crease-26cd1.firebaseapp.com",
+  projectId: "the-crease-26cd1",
+  storageBucket: "the-crease-26cd1.firebasestorage.app",
+  messagingSenderId: "621192972913",
+  appId: "1:621192972913:web:7e74da228a8a3a87b1a9d0"
 };
