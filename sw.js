0f62aedc2980de4fbc05ac133a9061316f99ce11
bubsmilo <bubsmilo@gmail.com>
2026-07-28 21:50:46 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='crease-v701';
+const CACHE='crease-v71';
 const ASSETS=['./','./index.html','./icon-192.png','./icon-512.png','./favicon.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
